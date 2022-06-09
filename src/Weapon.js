3e181ec643b89d12492b0bc5ed9e9dfff2eff29f
Tamajin <wild.jlq@gmail.com>
2022-06-09 14:51:03 +0200

    created class Weapon

--- src/Weapon.js
+++ src/Weapon.js
@@ -1,7 +1,8 @@
 class Weapon{
-    constructor()
+    constructor(name, damage = 10){
+        this.name = name;
+        this.damage = damage;
+    }
 }
 
-
-
-*module.exports = Weapon;
+module.exports = Weapon;
